c384277ec748ba09c0b0251bc7dcee30963f567d
Yuto Takano <moa17stock@gmail.com>
2025-10-15 14:26:06 +0200

    Remove React and Vite logo and unused CSS styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 
@@ -16,17 +15,6 @@ function App() {
     <main className="container">
       <h1>Welcome to Tauri + React</h1>
 
-      <div className="row">
-        <a href="https://vite.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
 
       <form
