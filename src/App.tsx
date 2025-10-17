7ed8e9811c47cf6b542f713ef58894880445e209
Yuto Takano <moa17stock@gmail.com>
2025-10-17 12:17:37 +0200

    Increase button matrix to 5x5, add proximity-hover effect using OS mouse position

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,61 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import "./App.css";
 import { Button } from "./components/Button";
 import { Icons } from "./components/Icon";
+import { cursorPosition, getCurrentWindow, primaryMonitor } from "@tauri-apps/api/window";
+
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
+
+  const [coord, setCoord] = useState({x: 0, y: 0});
+  const [winCoord, setWinCoord] = useState({x: 0, y: 0});
+  const localCoord = {
+    x: (coord.x - winCoord.x),
+    y: (coord.y - winCoord.y),
+  };
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  useEffect(() => {
+    const id = setInterval(async () => {
+      const scale = await getCurrentWindow().scaleFactor();
+      const pos = (await cursorPosition()).toLogical(scale);
+      const win = (await getCurrentWindow().outerPosition()).toLogical(scale);
+      setCoord({x: pos.x, y: pos.y});
+      setWinCoord({x: win.x, y: win.y});
+    }, 10);
+
+    return () => {
+      clearInterval(id);
+    }
+  }, []);
+
   return (
     <main className="container">
-      <div style={{ display: "flex", flexDirection: "row", gap: "9px" }}>
-      <Button onClick={() => greet()}>
-        <Icons.minMac width={8} height={8} />
-      </Button>
-      <Button onClick={() => greet()}>
-        <Icons.fullMac width={6} height={6} />
-      </Button>
-      <Button onClick={() => greet()}>
-        <Icons.closeMac width={8} height={8} />
-      </Button>
+      <div style={{ display: "flex", flexDirection: "column", gap: "9px" }}>
+        {[...Array(5)].map((_, i) => (
+          <div key={i} style={{ display: "flex", flexDirection: "row", gap: "9px" }}>
+            {[...Array(5)].map((_, j) => (
+              <Button key={i * 5 + j} onClick={() => greet()} currentMouseX={localCoord.x} currentMouseY={localCoord.y}>
+                <Icons.closeMac width={8} height={8} />
+              </Button>
+            ))}
+          </div>
+        ))}
       </div>
       <h1>Welcome to Tauri + React</h1>
 
       <p>Click on the Tauri, Vite, and React logos to learn more.</p>
+
+      <p>Coordinates: X: ${coord.x} Y: ${coord.y}</p>
+      <p>Window: X: ${winCoord.x} Y: ${winCoord.y}</p>
+      <p>Local: X: ${localCoord.x} Y: ${localCoord.y}</p>
 
       <input
         id="greet-input"
--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,18 +1,37 @@
-import type { ButtonHTMLAttributes } from "react";
+import { useCallback, useEffect, useRef, useState, type ButtonHTMLAttributes } from "react";
 import clsx from "clsx";
 import classes from "./Button.module.css"
 
+interface WindowButtonProps {
+  currentMouseX: number,
+  currentMouseY: number,
+}
+
 export const Button = ({
   className,
+  currentMouseX,
+  currentMouseY,
   children,
   ...props
-}: ButtonHTMLAttributes<HTMLButtonElement>) => {
+}: ButtonHTMLAttributes<HTMLButtonElement> & WindowButtonProps) => {
+  const [boundingClientRect, setBoundingClientRect] = useState<DOMRect>(new DOMRect());
+  const handleRect = useCallback((node: HTMLButtonElement | null) => {
+    if (node) {
+      setBoundingClientRect(node.getBoundingClientRect());
+    }
+  }, []);
+
+  const midX = boundingClientRect.left + boundingClientRect.width / 2;
+  const midY = boundingClientRect.top + boundingClientRect.height / 2;
+  const isMouseNear = Math.hypot(currentMouseX - midX, currentMouseY - midY) < 100
+
   return (
     <button
       className={clsx(classes.button, className)}
+      ref={handleRect}
       {...props}
     >
-      {children}
+      {isMouseNear ? <div style={{ opacity: isMouseNear ? (130 - Math.hypot(currentMouseX - midX, currentMouseY - midY)) / 100 : 1}}>{children}</div> : undefined}
     </button>
   )
 }
